381d20fcc10915475588c175ced56a5fe8756abe
gabrielpastrana <gabrielv.pastrana@gmail.com>
2022-05-04 20:00:12 -0300

    aula 04/05

--- 14-bd.php
+++ 14-bd.php
@@ -1,8 +1,30 @@
 <?php
 $servername = "localhost";
-$username = "username";
-$password = "password";
-$dbname = "myDB";
+$username = "root";
+$password = "";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password);
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+// Create database
+$sql = "CREATE DATABASE 5SI";
+if (mysqli_query($conn, $sql)) {
+  echo "Database created successfully<br>";
+} else {
+  echo "Error creating database: " . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+?>
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "5SI";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
@@ -12,15 +34,14 @@ if (!$conn) {
 }
 
 // sql to create table
-$sql = "CREATE TABLE MyGuests (
+$sql = "CREATE TABLE Pessoa (
 idPessoa INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-nomePessoa VARCHAR(60) NOT NULL,
-email VARCHAR(50),
-reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-)";
+nomePessoa VARCHAR(30) NOT NULL,
+emailPessoa VARCHAR(30) NOT NULL,
+idadePessoa int not null)";
 
 if (mysqli_query($conn, $sql)) {
-  echo "Table MyGuests created successfully";
+  echo "Table Pessoa created successfully";
 } else {
   echo "Error creating table: " . mysqli_error($conn);
 }
